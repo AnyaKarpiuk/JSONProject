da45db60e29bf1cc47cadc1beff437e1d1df6320
AnyaKarpiuk <anna-19.1994@mail.ru>
2019-12-08 01:07:14 +0000

    Change folder

---
+++ Design/jquery/table.js
@@ -0,0 +1,31 @@
+var coffee = [
+
+    {name: 'Macchiato', subname: 'Single', shotTime: 24, cupSize: 100, foamAmount: 10, 
+    instruction: "Extract single shot of coffee. Steam milk. Add few spoons of foam on a top."},
+
+    {name: 'Macchiato', subname: 'Double', shotTime: 24, cupSize: 150, foamAmount: 10, 
+    instruction: "Extract double shot of coffee. Steam milk. Add few spoons of foam on a top."},
+
+    {name: 'Ristretto', shotTime: 17, cupSize: 100,  
+    instruction: "When extraction double shot of coffee press a button stop after 17 seconds."},
+
+    {name: 'Americano', subname: 'Black', shotTime: 24, cupSize: 300, waterAmount: 70, 
+    instruction: "Extract double shot of coffee. Add hot water."},
+
+    {name: 'Americano', subname: 'White', shotTime: 24, cupSize: 300, waterAmount: 70, milkAmount: 5,
+    instruction: "Extract double shot of coffee. Add hot water. Add splash of hot milk."},
+
+    {name: 'Latte', shotTime: 24, cupSize: 300, milkAmount: 50, foamAmount: 20,
+    instruction: "Extract double shot of coffee. Steam milk, leaving a steamer on a top for 3 seconds. Pour milk in a cup and after slowly add a shot of coffee."},
+
+    {name: 'Mocha', shotTime: 24, cupSize: 300, milkAmount: 50, foamAmount: 20, chocolate: 2,
+    instruction: "Extract double shot of coffee and mix it with chocolate until it dissolves. Steam milk, leaving a steamer on a top for 3 seconds. Pour milk in a cup with coffee and chocolate."}
+
+];
+
+
+
+
+
+
+
